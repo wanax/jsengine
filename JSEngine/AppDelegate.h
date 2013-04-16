66a4f7f6d3290ce68509257f1fa76aead29a06d0
wanax <mxiaochi@gmail.com>
2013-04-16 10:37:26 +0800

    Initial Commit

---
+++ JSEngine/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  JSEngine
+//
+//  Created by Xcode on 13-4-16.
+//  Copyright (c) 2013年 Xcode. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ JSEngine/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  JSEngine
+//
+//  Created by Xcode on 13-4-16.
+//  Copyright (c) 2013年 Xcode. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
